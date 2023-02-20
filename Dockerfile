3ad12c80305156f2cba1d8800a71a91a68ec7e72
Simon Tucker <brotherlogic@gmail.com>
2023-02-19 16:45:49 -0800

    First pass at shell for rstore. This closes #6

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+# syntax=docker/dockerfile:1
+
+FROM golang:1.18 AS build
+
+WORKDIR $GOPATH/src/github.com/brotherlogic/rstore
+
+COPY go.mod ./
+COPY go.sum ./
+
+RUN mkdir proto
+COPY proto/*.go ./proto/
+
+RUN mkdir server
+COPY server/*.go ./server/
+
+RUN go mod download
+
+COPY *.go ./
+
+RUN CGO_ENABLED=0 go build -o /rstore
+
+##
+## Deploy
+##
+FROM gcr.io/distroless/base-debian11
+
+WORKDIR /
+
+COPY --from=build /rstore /rstore
+
+EXPOSE 8080
+EXPOSE 8081
+
+USER nonroot:nonroot
+
+ENTRYPOINT ["/rstore"]
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"google.golang.org/grpc"
+)
+
+var (
+	port        = flag.Int("port", 8080, "The server port.")
+	metricsPort = flag.Int("metrics_port", 8081, "Metrics port")
+)
+
+func main() {
+	flag.Parse()
+
+	//s := &server.Server{}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen on the port %v: %v", *port, err)
+	}
+	gs := grpc.NewServer()
+	log.Printf("server listening at %v", lis.Addr())
+
+	// Setup prometheus export
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		http.ListenAndServe(fmt.Sprintf(":%v", *metricsPort), nil)
+	}()
+
+	if err := gs.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
