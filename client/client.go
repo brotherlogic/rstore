736d93960b959f9b5a9e98877bec9dcaf6881923
Simon Tucker <brotherlogic@gmail.com>
2023-04-02 08:53:26 -0700

    Adds rstore client

---
+++ client/client.go
@@ -0,0 +1,49 @@
+package rstore_client
+
+import (
+	"context"
+
+	pb "github.com/brotherlogic/rstore/proto"
+	"google.golang.org/grpc"
+)
+
+type RStoreClient struct {
+	test    bool
+	gClient pb.RStoreServiceClient
+}
+
+func GetClient() (*RStoreClient, error) {
+	conn, err := grpc.Dial("rstore.rstore:80", grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return &RStoreClient{gClient: pb.NewRStoreServiceClient(conn)}, nil
+}
+
+func GetTestClient() (*RStoreClient, error) {
+	return &RStoreClient{test: true}, nil
+}
+
+func (c *RStoreClient) Read(ctx context.Context, req *pb.ReadRequest) (*pb.ReadResponse, error) {
+	if c.test {
+		return &pb.ReadResponse{}, nil
+	}
+
+	return c.gClient.Read(ctx, req)
+}
+
+func (c *RStoreClient) Write(ctx context.Context, req *pb.WriteRequest) (*pb.WriteResponse, error) {
+	if c.test {
+		return &pb.WriteResponse{}, nil
+	}
+
+	return c.gClient.Write(ctx, req)
+}
+
+func (c *RStoreClient) GetKeys(ctx context.Context, req *pb.GetKeysRequest) (*pb.GetKeysResponse, error) {
+	if c.test {
+		return &pb.GetKeysResponse{}, nil
+	}
+
+	return c.gClient.GetKeys(ctx, req)
+}
