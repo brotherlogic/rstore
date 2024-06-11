f08e51f44acb6945fcba63366f724972ef18ffb0
Simon <brotherlogic@gmail.com>
2024-06-11 13:29:08 -0700

    Adds the redis client

---
+++ redis.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	pb "github.com/brotherlogic/rstore/proto"
+	"github.com/redis/go-redis/v9"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+type redisClient struct {
+	rdb *redis.Client
+}
+
+func (r *redisClient) Read(ctx context.Context, req *pb.ReadRequest) (*pb.ReadResponse, error) {
+	/*if val, ok := s.cache[req.GetKey()]; ok {
+		return &pb.ReadResponse{Value: &anypb.Any{Value: val}}, nil
+	}*/
+	cmd := r.rdb.Get(ctx, req.GetKey())
+	result, err := cmd.Bytes()
+
+	if err == redis.Nil {
+		return nil, status.Errorf(codes.NotFound, "key %v was not found", req.GetKey())
+	}
+
+	if err != nil {
+		log.Printf("remote err on read: %v", err)
+	}
+	return &pb.ReadResponse{Value: &anypb.Any{Value: result}}, err
+}
+
+func (r *redisClient) Write(ctx context.Context, req *pb.WriteRequest) (*pb.WriteResponse, error) {
+	err := r.rdb.Set(ctx, req.GetKey(), req.GetValue().GetValue(), 0).Err()
+	if err != nil {
+		log.Printf("remote err on write: %v", err)
+	} else {
+		//s.cache[req.GetKey()] = req.GetValue().GetValue()
+	}
+	return &pb.WriteResponse{}, err
+}
+
+func (r *redisClient) GetKeys(ctx context.Context, req *pb.GetKeysRequest) (*pb.GetKeysResponse, error) {
+	var akeys []string
+	t := time.Now()
+	defer func(t time.Time) {
+		log.Printf("Completed %v in %v", req.GetPrefix(), time.Since(t))
+	}(t)
+	iter := r.rdb.Scan(ctx, 0, fmt.Sprintf("%v*", req.GetPrefix()), 1000).Iterator()
+
+	for iter.Next(ctx) {
+		key := iter.Val()
+		if req.GetAllKeys() || strings.Count(key, "/") == strings.Count(req.GetPrefix(), "/") {
+			valid := true
+			for _, suffix := range req.GetAvoidSuffix() {
+				if strings.HasSuffix(key, suffix) {
+					valid = false
+				}
+			}
+			if valid {
+				akeys = append(akeys, key)
+			}
+		}
+	}
+
+	if err := iter.Err(); err != nil {
+		log.Printf("Failed to read keys (%v) in %v", req.GetPrefix(), time.Since(t))
+		return nil, fmt.Errorf("database error reading keys %w", err)
+	}
+
+	log.Printf("returning %v items (%v)", len(akeys), req.GetPrefix())
+	return &pb.GetKeysResponse{Keys: akeys}, nil
+}
+
+func (r *redisClient) Delete(ctx context.Context, req *pb.DeleteRequest) (*pb.DeleteResponse, error) {
+	return &pb.DeleteResponse{}, r.rdb.Del(ctx, req.GetKey()).Err()
+}
