ab95faa09b7c6e2273392c8ee572f7e1bf076288
Simon <brotherlogic@gmail.com>
2024-06-27 15:52:11 -0700

    Adds script

---
+++ scripts/builder.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/brotherlogic/rstore/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.Dial("kclust1:31791",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(1000*1024*1024)))
+	client := pb.NewRStoreServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Hour)
+	defer cancel()
+
+	log.Printf("Getting Keys")
+	keys, err := client.GetKeys(ctx, &pb.GetKeysRequest{AllKeys: true})
+
+	if err != nil {
+		log.Fatalf("Bad keys: %v", err)
+	}
+
+	log.Printf("Found %v keys", len(keys.GetKeys()))
+}
